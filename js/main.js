b2d40553eb72c3ea863a9e1cab76f623f2ca433e
sichevmi <sichevmi@gmail.com>
2015-02-05 16:01:43 +0300

    deleted:    404.html 	deleted:    ac0f17de784c.html 	deleted:    c1c23becb8ad.html 	new file:   css/bootstrap-theme.css 	new file:   css/bootstrap-theme.min.css 	new file:   css/bootstrap.css 	modified:   css/bootstrap.min.css 	deleted:    css/cover.css 	deleted:    css/main.css 	new file:   css/onepage-scroll.css 	new file:   css/pushy.css 	new file:   css/style.css 	modified:   fonts/glyphicons-halflings-regular.eot 	modified:   fonts/glyphicons-halflings-regular.svg 	modified:   fonts/glyphicons-halflings-regular.ttf 	modified:   fonts/glyphicons-halflings-regular.woff 	deleted:    img/.gitignore 	modified:   index.html 	new file:   js/bootstrap.js 	new file:   js/bootstrap.min.js 	new file:   js/jquery.onepage-scroll.min.js 	new file:   js/jquery.scrollto.js 	new file:   js/main.js 	new file:   js/pushy.min.js 	new file:   pushy-master/Gruntfile.js 	new file:   pushy-master/LICENSE.txt 	new file:   pushy-master/README.md 	new file:   pushy-master/bower.json 	new file:   pushy-master/css/.DS_Store 	new file:   pushy-master/css/demo.css 	new file:   pushy-master/css/normalize.css 	new file:   pushy-master/css/pushy.css 	new file:   pushy-master/demo.html 	new file:   pushy-master/js/pushy.js 	new file:   pushy-master/js/pushy.min.js 	new file:   pushy-master/package.json 	deleted:    robots.txt

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Плавный переход по якорям
+$('a[href^="#"]').click(function(){
+//Сохраняем значение атрибута href в переменной:
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+return false;
+});
